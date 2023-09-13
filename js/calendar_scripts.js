479df0a42b270f146bfc3e43687e04a7ac3aef14
Subhodeep Dey <90602098+subhodeep-dey@users.noreply.github.com>
2023-09-13 13:08:20 +0530

    Update calendar_scripts.js

--- js/calendar_scripts.js
+++ js/calendar_scripts.js
@@ -1,36 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const passcodeInput = document.getElementById("passcode-input");
-    const submitButton = document.getElementById("submit-passcode");
-    const contentDiv = document.getElementById("content");
-    const passcodeContainer = document.getElementById("passcode-container");
+// Get references to the input field and the form
+const passcodeInput = document.getElementById('passcode-input');
+const passcodeForm = document.getElementById('passcode-form');
 
-    // Define the correct passcode (change this to your desired passcode)
-    const correctPasscode = "subhodeepdey";
+// Add a submit event listener to the form
+passcodeForm.addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the default form submission
 
-    // Add an event listener to the submit button
-    submitButton.addEventListener("click", function () {
-        const enteredPasscode = passcodeInput.value;
-
-        if (enteredPasscode === correctPasscode) {
-            // If the entered passcode is correct, show the content
-            contentDiv.style.display = "block";
-            passcodeContainer.style.display = "none"; // Hide the passcode input
-        } else {
-            // If the entered passcode is incorrect, display an error message
-            alert("Incorrect passcode. Please try again.");
-            passcodeInput.value = ""; // Clear the input field
-        }
-    });
+    // Trigger the button click event
+    document.getElementById('submit-passcode').click();
 });
 
-// Get references to the input field and the form
-    const passcodeInput = document.getElementById('passcode-input');
-    const passcodeForm = document.getElementById('passcode-form');
+// Add an event listener to the submit button
+document.getElementById('submit-passcode').addEventListener('click', function () {
+    const enteredPasscode = passcodeInput.value;
+    const correctPasscode = "subhodeepdey"; // Change to your desired passcode
 
-    // Add a submit event listener to the form
-    passcodeForm.addEventListener('submit', function (e) {
-        e.preventDefault(); // Prevent the default form submission
-
-        // Trigger the button click event
-        document.getElementById('submit-passcode').click();
-    });
+    if (enteredPasscode === correctPasscode) {
+        // If the entered passcode is correct, show the content
+        contentDiv.style.display = "block";
+        passcodeContainer.style.display = "none"; // Hide the passcode input
+    } else {
+        // If the entered passcode is incorrect, display an error message
+        alert("Incorrect passcode. Please try again.");
+        passcodeInput.value = ""; // Clear the input field
+    }
+});
